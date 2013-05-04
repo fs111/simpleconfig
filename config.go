4b345d2b1f5030cd409f76fa0a15bac1918185ef
Andre Kelpe <efeshundertelf@googlemail.com>
2013-05-04 15:40:20 +0200

    initial import of simple config parser extracted from kurz.go

---
+++ config.go
@@ -0,0 +1,97 @@
+package simpleconfig
+//a package for handling simple config files in key=value format.
+//the code treats # as the comment character
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+const (
+	COMMENT   = "#"
+	SEPARATOR = "="
+)
+
+//struct representing a config file
+// which are simple key=value entries
+type Config struct {
+	Path    string
+	Entries map[string]string
+}
+
+//Creates a new config struct for the given path
+// which is assumed to be a file on the file system
+func NewConfig(path string) (c *Config, err error){
+	cfg := new(Config)
+	cfg.Path = path
+	cfg.Entries = make(map[string]string)
+    parseError := cfg.Parse()
+	return cfg, parseError
+}
+
+//Parses the configuration file and stores all key-value
+//pairs in the Entries map
+func (c Config) Parse() error {
+	contents, err := ioutil.ReadFile(c.Path)
+	if err != nil {
+		return err
+	}
+	for _, line := range strings.Split(string(contents), "\n") {
+		line = strings.TrimSpace(line)
+		if len(line) > 2 && !strings.HasPrefix(line, COMMENT) {
+			row := strings.SplitN(line, SEPARATOR, 2)
+			key, value := strings.TrimSpace(string(row[0])),
+				strings.TrimSpace(string(row[1]))
+			c.Entries[key] = value
+		}
+	}
+	return nil
+}
+
+// Returns the config entry belonging to the given key, if present,
+// otherwise err will be not nil.
+func (c Config) GetString(key string) (entry string, err error) {
+	value, ok := c.Entries[key]
+	var e error
+	if !ok {
+		msg := fmt.Sprintf("unknown configuration entry '%s'", key)
+		e = errors.New(msg)
+	} else {
+		e = nil
+	}
+	return value, e
+}
+
+//Returns the value for the given key, if it is present, otherwise
+//the val is returned.
+func (c Config) GetStringDefault(key string, val string) string {
+	entry, e := c.GetString(key)
+	if e == nil {
+		return entry
+	}
+	return val
+}
+
+//Returns the value stored under the key as an int if present, otherwise
+//err is set to non nil
+func (c Config) GetInt(key string) (val int, err error) {
+	entry, e := c.GetString(key)
+	if e == nil {
+		return strconv.Atoi(entry)
+	}
+	msg := fmt.Sprintf("unknown configuration entry '%s'", key)
+	return -1, errors.New(msg)
+}
+
+// Returns the value stored under key as an int, if no such entry exists
+// it will return the given val.
+func (c Config) GetIntDefault(key string, val int) int {
+	entry, e := c.GetInt(key)
+	if e == nil {
+		return entry
+	}
+	return val
+}
---
+++ config_test.go
@@ -0,0 +1,116 @@
+package simpleconfig
+
+import (
+    "testing"
+    "io/ioutil"
+    "os"
+)
+
+func Test_NewConfig(t *testing.T){
+
+    c, err := NewConfig("/dev/null")
+    if err != nil{
+        t.Error("creating new config struct failed")
+    }
+    if c == nil{
+        t.Error("creating new config struct failed")
+    }
+}
+
+
+func Test_NewConfigParsing(t *testing.T){
+
+    tmpFile := MakeConfigFile("\n#answer=42\nfoo=bar\n\n")
+    defer os.Remove(tmpFile.Name())
+
+    c, err := NewConfig(tmpFile.Name())
+    if err != nil{
+        t.Error("creating new config struct failed")
+    }
+    if c == nil{
+        t.Error("creating new config struct failed")
+    }
+
+    answer, e := c.GetInt("answer")
+    if e == nil{
+        t.Error("parsing of comments does not work")
+    }
+    if answer != -1{
+        t.Error("got an unexpexted value")
+    }
+
+    foo, _ := c.GetString("foo")
+    if foo != "bar"{
+        t.Error("the value of foo should be bar")
+    }
+}
+
+
+func Test_GetStringEmptyFile(t *testing.T){
+    c, _ := NewConfig("/dev/null")
+    _, err := c.GetString("test")
+    if err == nil {
+        t.Error("an error for an non existing key should have been returned")
+    }
+}
+
+func Test_GetStringDefaultEmptyFile(t *testing.T){
+    c, _ := NewConfig("/dev/null")
+    defaultValue := "default"
+    val := c.GetStringDefault("test", defaultValue)
+    if val != defaultValue{
+        t.Error("the default value should have been returned")
+    }
+}
+
+func Test_GetStringWithExistingKeys(t *testing.T){
+    tmpFile := MakeConfigFile("foo=bar\nbla=1")
+    defer os.Remove(tmpFile.Name())
+
+    c, _ := NewConfig(tmpFile.Name())
+    val, _ := c.GetString("foo")
+    if val != "bar"{
+        t.Error("the key 'foo' does not have the value 'bar'")
+    }
+    val, _ = c.GetString("bla")
+    if val != "1"{
+        t.Error("the key 'bla' does not have the value '1'")
+    }
+}
+
+func Test_GetIntEmptyFile(t *testing.T){
+    c, _ := NewConfig("/dev/null")
+    _, err := c.GetInt("test")
+    if err == nil {
+        t.Error("an error for an non existing key should have been returned")
+    }
+}
+
+func Test_GetIntDefaultEmptyFile(t *testing.T){
+    c, _ := NewConfig("/dev/null")
+    defaultValue := 42
+    val := c.GetIntDefault("test", defaultValue)
+    if val != defaultValue{
+        t.Error("the default value should have been returned")
+    }
+}
+
+func Test_GetInt(t *testing.T){
+
+    tmpFile := MakeConfigFile("answer=42")
+    defer os.Remove(tmpFile.Name())
+
+    c, _ := NewConfig(tmpFile.Name())
+    val, _ := c.GetInt("answer")
+    if val != 42{
+        t.Error("the answer is 42")
+    }
+}
+
+
+func MakeConfigFile(content string) *os.File{
+    tmpFile, _ := ioutil.TempFile("", "simpleconfig")
+    tmpFile.WriteString(content)
+    tmpFile.Close()
+    return tmpFile
+}
